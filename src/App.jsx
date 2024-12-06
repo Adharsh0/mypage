88a7a6069a1c08551f2580bcd67fd566356b7bf4
Adharsh0 <118553385+Adharsh0@users.noreply.github.com>
2024-12-06 21:34:00 +0530

    change34

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,4 @@
 
-// import React, { useState } from 'react';
-// import './App.css';
-// import HeroSection from './components/HeroSection';
-// import SkillSection from './components/SkillSection';
-// import ProjectSection from './components/ProjectSection';
-// import Profile from './components/Profile';
-// import ContactForm from './components/ContactForm';
-
-
-// function App() {
-//   const [showProfile, setShowProfile] = useState(false);
-
-//   const handleProfileClick = () => {
-//     setShowProfile(true); // Show the profile when the button is clicked
-//   };
-
-//   const handleBackToHero = () => {
-//     setShowProfile(false); // Go back to Hero section
-//   };
-
-//   return (
-//     <div className="App">
-//       {!showProfile ? (
-//         <>
-//           <HeroSection onProfileClick={handleProfileClick} />
-//           <SkillSection />
-//           <ProjectSection />
-//           <ContactForm />
-//         </>
-//       ) : (
-//         <div>
-//           <Profile />
-//           {/* Back button to return to Hero Section */}
-//           <button onClick={handleBackToHero} className="back-to-hero-btn">Back to Home</button>
-          
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
 import React, { useState } from 'react';
 import './App.css';
 import HeroSection from './components/HeroSection';
@@ -49,6 +7,7 @@ import ProjectSection from './components/ProjectSection';
 import Profile from './components/Profile';
 import ContactForm from './components/ContactForm';
 import EndSection from './components/EndSection';
+
 
 function App() {
   const [showProfile, setShowProfile] = useState(false);
@@ -65,15 +24,18 @@ function App() {
     <div className="App">
       {!showProfile ? (
         <>
-          <HeroSection id="home" onProfileClick={handleProfileClick} />
+          <HeroSection onProfileClick={handleProfileClick} />
           <SkillSection />
           <ProjectSection />
-          <ContactForm id="contact" />
+          <ContactForm />
           <EndSection />
         </>
       ) : (
         <div>
-          <Profile onBackToHero={handleBackToHero} />
+          <Profile />
+          {/* Back button to return to Hero Section */}
+          <button onClick={handleBackToHero} className="back-to-hero-btn">Back to Home</button>
+          
         </div>
       )}
     </div>
